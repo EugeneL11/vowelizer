42e212cd3f0f3b75726ca7617dc4186a9dd9d8c3
EugeneL11 <eugenelee0605@gmail.com>
2023-11-15 19:36:18 -0700

    Implement all functionality

--- test.cpp
+++ test.cpp
@@ -11,17 +11,26 @@
 #include <cstdlib> //can i use this to use exit()?
 
 //define port to use
-#define PORT 9001
+#define PORT 9011
+#define PORT2 9012
 
 using namespace std;
 
 int main() {
-    int client_fd = socket(AF_INET, SOCK_STREAM, 0); //socket to interact with server
+    //TCP socket
+    int tcpSock = socket(AF_INET, SOCK_STREAM, 0); //socket to interact with server
     struct sockaddr_in serv_addr; 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT); //port number of socket
     inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr); //IP address of socket   
-    connect(client_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)); //connect socket to server
+    connect(tcpSock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)); //connect socket to server
+
+    //UDP socket
+    int udpSock = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in serv_addr2;
+    serv_addr2.sin_family = AF_INET;
+    serv_addr2.sin_port = htons(PORT2);
+    serv_addr2.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     while (true) {
         //menu to display to user
@@ -33,49 +42,76 @@ int main() {
             "- Merge Text (advanced encoding)   [Press '4']\n"
             "- Quit                             [Press '5']\n\n"
             "Which option would you like to choose: ";
-        string choice;
+        string choice = "";
         cout << menuStr;
         getline(cin, choice);
-        string splitStr;
-        string mergeVowel;
-        string mergeConsonant;
+        string splitStr = "";
+        string splitStr2 = "";
+        string mergeVowel = "";
+        string mergeConsonant = "";
+        string mergeConsonant2 = "";
         string closeMsg = "Exiting...";
+        char buffer[1024] = {0};
+        int bytesRec;
+        char buffer2[1024] = {0};
+        string recStr = "";
+        string none = "a";
 
         if (choice == "1") {
             cout << "\nSplitting Text (basic encoding)" << endl;
             cout << "Please enter a string to split into consonants and vowels: ";
             getline(cin, splitStr);
-            send(client_fd, splitStr.c_str(), splitStr.length(), 0);
+            splitStr2 = "$bs1" + splitStr; //add the identifier
+            send(tcpSock, splitStr2.c_str(), splitStr2.length(), 0);
+            //sending using UDP just so the server can have client address
+            ssize_t bytesSent = sendto(udpSock, none.c_str(), none.length(), 0, (struct sockaddr *)&serv_addr2, sizeof(serv_addr2));
+            bytesRec = recv(tcpSock, buffer, sizeof(buffer), 0);
+            cout << "Split Consonant String (Basic): " << buffer << endl;
+            ssize_t udpBytesRec = recvfrom(udpSock, buffer2, sizeof(buffer2), 0, nullptr, nullptr);
+            cout << "Split Vowel String(Basic): " << buffer2 << endl << endl;
 
         } else if (choice == "2") {
             cout << "\nMerging Text (basic encoding)" << endl;
             cout << "Please enter a consonant string to merge: ";
             getline(cin, mergeConsonant);
-            send(client_fd, mergeConsonant.c_str(), mergeConsonant.length(), 0);
+            mergeConsonant2 = "$bm1" + mergeConsonant; //add the identifier
+            send(tcpSock, mergeConsonant2.c_str(), mergeConsonant2.length(), 0);
             cout << "Please enter a vowel string to merge: ";
             getline(cin, mergeVowel); //send over UDP
-            send(client_fd, mergeVowel.c_str(), mergeVowel.length(), 0);
+            ssize_t bytesSent = sendto(udpSock, mergeVowel.c_str(), mergeVowel.length(), 0, (struct sockaddr *)&serv_addr2, sizeof(serv_addr2));
+            
+            bytesRec = recv(tcpSock, buffer, sizeof(buffer), 0);
+            cout << "Merged String (Basic): " << buffer << endl << endl;
 
         } else if (choice == "3") {
             cout << "\nSplitting Text (advanced encoding)" << endl;
             cout << "Please enter a string to split into consonants and vowels: ";
-            //cin >> splitStr;
             getline(cin, splitStr);
-            cout << splitStr;
-            send(client_fd, splitStr.c_str(), splitStr.length(), 0);
+            splitStr2 = "$as2" + splitStr; //add the identifier
+            send(tcpSock, splitStr2.c_str(), splitStr2.length(), 0);
+            //sending using UDP just so the server can have client address
+            ssize_t bytesSent = sendto(udpSock, none.c_str(), none.length(), 0, (struct sockaddr *)&serv_addr2, sizeof(serv_addr2));
+            bytesRec = recv(tcpSock, buffer, sizeof(buffer), 0);
+            cout << "Split Consonant String (Advanced): " << buffer << endl;
+            ssize_t udpBytesRec = recvfrom(udpSock, buffer2, sizeof(buffer2), 0, nullptr, nullptr);
+            cout << "Split Vowel String (Advanced): " << buffer2 << endl << endl;
 
         } else if (choice == "4") {
             cout << "\nMerging Text (advanced encoding)" << endl;
             cout << "Please enter a consonant string to merge: ";
             getline(cin, mergeConsonant);
-            send(client_fd, mergeConsonant.c_str(), mergeConsonant.length(), 0);
+            mergeConsonant2 = "$am2" + mergeConsonant; //add the identifier
+            send(tcpSock, mergeConsonant2.c_str(), mergeConsonant2.length(), 0);
             cout << "Please enter a vowel string to merge: ";
             getline(cin, mergeVowel); //send over UDP
-            send(client_fd, mergeVowel.c_str(), mergeVowel.length(), 0);
+            ssize_t bytesSent = sendto(udpSock, mergeVowel.c_str(), mergeVowel.length(), 0, (struct sockaddr *)&serv_addr2, sizeof(serv_addr2));
+
+            bytesRec = recv(tcpSock, buffer, sizeof(buffer), 0);
+            cout << "Merged String (Advanced): " << buffer << endl << endl;
 
         } else if (choice == "5") {
             cout << "\nThank you for using vowelizer! See you next time..." << endl;
-            send(client_fd, closeMsg.c_str(), closeMsg.length(), 0);
+            send(tcpSock, closeMsg.c_str(), closeMsg.length(), 0);
             break;
             
         } else {
@@ -83,6 +119,8 @@ int main() {
             //cin >> choice;
         }
     }
-    close(client_fd);
+    //close both sockets
+    close(tcpSock);
+    close(udpSock);
     return 0;
 }
--- vowelizerServer.cpp
+++ vowelizerServer.cpp
@@ -8,33 +8,144 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define PORT 9001
+#define PORT 9011
+#define PORT2 9012
 
 using namespace std;
 int main() {
+    //for TCP socket
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in address;
     int alen = sizeof(address);
     address.sin_family = AF_INET; //for IPv4
     inet_pton(AF_INET, "127.0.0.1", &(address.sin_addr));
     address.sin_port = htons(PORT);
-
     bind(server_fd, (struct sockaddr*)&address, alen);
     listen(server_fd, 3); //max 3 clients
-
-    cout << "Waiting for new connection..." << endl;
-    
+    cout << "Waiting for new TCP connection..." << endl;
     int accepted = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&alen);
-    cout << "Connection accepted!" << endl;
+    cout << "TCP Connection accepted!" << endl;
 
-    char buffer[1024] = {0};
-    int bytesRec = 0;
+    //for UDP socket
+    int udpSock = socket(AF_INET, SOCK_DGRAM, 0);
+    struct sockaddr_in udpAddr;
+    udpAddr.sin_family = AF_INET;
+    udpAddr.sin_addr.s_addr = INADDR_ANY;
+    udpAddr.sin_port = htons(PORT2);
+    if (bind(udpSock, (struct sockaddr*)&udpAddr, sizeof(udpAddr)) == -1) {
+        cout << "UDP Bind Failed!" << endl;
+        return -1;
+    }
 
     while (1) {
+        char buffer[1024] = {0};
+        int bytesRec = 0;
+
+        struct sockaddr_in clientAddr;
+        socklen_t clientAddrLen = sizeof(clientAddr);
+        char buffer2[1024] = {0};
+
+        char vowels[] = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        string vowelStr = "";
+        string consonantStr = "";
+
         bytesRec = recv(accepted, buffer, sizeof(buffer), 0);
         cout << "Received: " << buffer << endl;
         string recStr(buffer, bytesRec);
-        if (recStr == "Exiting...") {
+        
+        //option one (basic split)
+        if (recStr.find("$bs1") != string::npos) { //if it has identifier
+            recStr = recStr.erase(0, 4); //remove identifier now
+            for (size_t i = 0; i < recStr.length(); ++i) { //loop through string
+                char c = recStr[i];
+                //check if the character is a vowel
+                if (find(begin(vowels), end(vowels), c) != end(vowels)) {
+                    vowelStr += c; //char is a vowel
+                    consonantStr += " "; //add spaces to represent consonants
+                } else {
+                    consonantStr += c; //char is a consonant
+                    vowelStr += " "; //add spaces to represent vowels
+                }
+            }
+            //send consonant string using TCP
+            send(accepted, consonantStr.c_str(), consonantStr.length(), 0);
+            //receive dummy message to resolve client address for UDP
+            ssize_t udpBytesRec = recvfrom(udpSock, buffer2, sizeof(buffer2), 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
+            //send vowel string using UDP
+            ssize_t bytesSent = sendto(udpSock, vowelStr.c_str(), vowelStr.length(), 0, (struct sockaddr *)&clientAddr, sizeof(clientAddr));
+        }
+
+        //option two (basic merge)
+        else if (recStr.find("$bm1") != string::npos) { //if it has identifier
+            recStr = recStr.erase(0, 4); //remove identifier now
+            cout << "Consonant String: " << recStr << endl;
+            ssize_t udpBytesRec = recvfrom(udpSock, buffer2, sizeof(buffer2), 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
+            cout << "Vowel String: " << buffer2 << endl;
+            string vowel(buffer2);
+            for (size_t i = 0; i < recStr.length(); ++i) {
+                if (recStr[i] == ' ') {
+                    recStr[i] = vowel[i];
+                }
+            }
+            send(accepted, recStr.c_str(), recStr.length(), 0);
+        }
+
+        //option three (advanced split)
+        else if (recStr.find("$as2") != string::npos) { //if it has identifier
+            int count = 0;
+            recStr = recStr.erase(0, 4); //remove identifier now
+            for (size_t i = 0; i < recStr.length(); ++i) { //loop through string
+                char c = recStr[i];
+                //check if the character is a vowel
+                if (find(begin(vowels), end(vowels), c) != end(vowels)) {
+                    if (count > 0) {
+                        vowelStr += to_string(count);
+                    }
+                    vowelStr += c; //char is a vowel
+                    count = 0; //reset the counter
+                } else {
+                    consonantStr += c; //char is a consonant
+                    count += 1; //increment consonant count
+                }
+            }
+            //send the consonant string over TCP
+            send(accepted, consonantStr.c_str(), consonantStr.length(), 0);
+            //receive dummy message to resolve client address for UDP
+            ssize_t udpBytesRec = recvfrom(udpSock, buffer2, sizeof(buffer2), 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
+            //send vowel string using UDP
+            ssize_t bytesSent = sendto(udpSock, vowelStr.c_str(), vowelStr.length(), 0, (struct sockaddr *)&clientAddr, sizeof(clientAddr));
+        }
+
+        //option four (advanced merge)
+        else if (recStr.find("$am2") != string::npos) { //if it has identifier
+            consonantStr = recStr.erase(0, 4); //remove identifier now
+            cout << "Consonant String: " << consonantStr << endl;
+            ssize_t udpBytesRec = recvfrom(udpSock, buffer2, sizeof(buffer2), 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
+            cout << "Vowel String: " << buffer2 << endl;
+            string vowel(buffer2);
+            string mergedStr = "";
+            //loop through the vowel string
+            for (size_t i = 0; i < vowel.length(); ++i) {
+                char c = vowel[i];
+                int cVal = c - '0';
+                //if it is a vowel
+                if (find(begin(vowels), end(vowels), c) != end(vowels)) {
+                    mergedStr += c;
+                } else { //if not a vowel
+                    mergedStr += consonantStr.substr(0, cVal);
+                    consonantStr.erase(0, cVal);
+                }
+                //on the last iteration, add remaining consonants
+                if (i == vowel.length() - 1) {
+                    mergedStr += consonantStr;
+                }
+            }
+            //send the merged string to client over TCP
+            send(accepted, mergedStr.c_str(), mergedStr.length(), 0);
+        }
+
+        // option five (exit)
+        else if (recStr == "Exiting...") {
             break;
         }
         // Clear the buffer to prepare for the next chunk
@@ -42,6 +153,7 @@ int main() {
     }
     close(accepted);
     close(server_fd);
+    close(udpSock);
 
     return 0;
 }
